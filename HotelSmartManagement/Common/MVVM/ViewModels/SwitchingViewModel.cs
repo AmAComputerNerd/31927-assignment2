58c75a50715cd2a8c79b58c31c83852babfd5d2a
Jonathon Thomson <80492761+AmAComputerNerd@users.noreply.github.com>
2024-08-08 22:28:21 +1000

    Write some basic code for project setup

---
+++ HotelSmartManagement/Common/MVVM/ViewModels/SwitchingViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using HotelSmartManagement.EmployeeSelfService.MVVM.ViewModels;
+
+namespace HotelSmartManagement.Common.MVVM.ViewModels
+{
+    public class SwitchingViewModel : ObservableObject
+    {
+        private object? _currentView;
+        public object? CurrentView { get => _currentView; set => SetProperty(ref _currentView, value); }
+
+        public SwitchingViewModel()
+        {
+            CurrentView = new ExampleViewModel();
+        }
+    }
+}
---
+++ HotelSmartManagement/EmployeeSelfService/MVVM/ViewModels/ExampleViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSmartManagement.EmployeeSelfService.MVVM.ViewModels
+{
+    public class ExampleViewModel : ObservableObject
+    {
+        private string? _helloWorld;
+        public string? HelloWorld { get => _helloWorld; set => SetProperty(ref _helloWorld, value); }
+
+        public ExampleViewModel()
+        {
+            HelloWorld = "Hello, World!";
+        }
+    }
+}
