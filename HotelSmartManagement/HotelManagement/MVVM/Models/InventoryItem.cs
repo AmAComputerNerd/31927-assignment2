625ddc5a008981090c25bb840cccc7c253ce509f
Lachlan Giang <lachlan.giang@wisetechglobal.com>
2024-08-13 17:54:18 +1000

    Models created + Removed SampleFile

---
+++ HotelSmartManagement/HotelManagement/MVVM/Models/InventoryItem.cs
@@ -0,0 +1,14 @@
+﻿using HotelSmartManagement.Common.Database.Misc;
+
+namespace HotelSmartManagement.HotelManagement.MVVM.Models
+{
+    public class InventoryItem : IDatabaseObject
+    {
+#nullable disable // Reason: Model for EF - expected that these properties do not get assigned in the constructor.
+        public Guid UniqueId { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        
+#nullable enable // Reason: Model for EF - expected that these properties do not get assigned in the constructor.
+    }
+}
