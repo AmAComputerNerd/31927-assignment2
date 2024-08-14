864d73a9c7a79f2d32352be99b04a8b12ab30b8b
Lachlan Giang <lachlan.giang@wisetechglobal.com>
2024-08-14 21:01:10 +1000

    Added ViewModels

---
+++ HotelSmartManagement/HotelManagement/MVVM/ViewModels/AddEventViewModel.cs
@@ -0,0 +1,40 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using HotelSmartManagement.Common.MVVM.Models;
+
+namespace HotelSmartManagement.HotelManagement.MVVM.ViewModels
+{
+    public class AddEventViewModel : ObservableRecipient
+    {
+        private Globals Globals { get; }
+
+        private Uri _imageUri;
+        public Uri ImageUri { get => _imageUri; set => SetProperty(ref _imageUri, value); }
+
+#nullable disable // Reason: _imageUri is set when we set ImageUri.
+        public AddEventViewModel(Globals globals)
+        {
+#nullable enable // Reason: _imageUri is set when we set ImageUri.
+            IsActive = true;
+
+            Globals = globals;
+            //ImageUri = Globals.GetProfilePictureUri();
+        }
+
+        protected override void OnActivated()
+        {
+            throw new NotImplementedException();
+        }
+        public void OnCancel()
+        {
+            throw new NotImplementedException();
+        }
+        public void OnSave()
+        {
+            throw new NotImplementedException();
+        }
+        public void ValidateFields()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
