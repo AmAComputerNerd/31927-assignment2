4e5243cc5a6bd6b7be776deb0052ade72c2096dc
Jonathon Thomson <80492761+AmAComputerNerd@users.noreply.github.com>
2024-08-15 22:16:06 +1000

    Create static page for EmployeeSelfServiceMyDetailsView. (#10)

---
+++ HotelSmartManagement/Common/XamlExtensions/UniformGridHelper.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace HotelSmartManagement.Common.XamlExtensions
+{
+    public static class UniformGridHelper
+    {
+        public static Thickness GetChildMargin(DependencyObject obj)
+        {
+            return (Thickness)obj.GetValue(ChildMarginProperty);
+        }
+
+        public static void SetChildMargin(DependencyObject obj, Thickness value)
+        {
+            obj.SetValue(ChildMarginProperty, value);
+        }
+
+        public static readonly DependencyProperty ChildMarginProperty =
+            DependencyProperty.RegisterAttached("ChildMargin", typeof(Thickness), typeof(UniformGridHelper),
+                new UIPropertyMetadata(new Thickness(), OnChildMarginChanged));
+
+        private static void OnChildMarginChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is UniformGrid grid)
+            {
+                grid.Loaded += (sender, args) =>
+                {
+                    foreach (UIElement child in grid.Children)
+                    {
+                        if (child is FrameworkElement fe)
+                        {
+                            fe.Margin = (Thickness)e.NewValue;
+                        }
+                    }
+                };
+            }
+        }
+    }
+}
