67fe7bd726df9628e2350f4b76f740ae32df7345
Lachlan Giang <92299122+LachlanGiang@users.noreply.github.com>
2024-08-17 12:49:55 +1000

    LGN/screen designs (#11)
    Co-authored-by: Lachlan Giang <lachlan.giang@wisetechglobal.com>

--- HotelSmartManagement/HotelOverview/MVVM/ViewModels/HotelManagementDashboardViewModel.cs
+++ HotelSmartManagement/HotelOverview/MVVM/ViewModels/HotelManagementDashboardViewModel.cs
@@ -2,10 +2,12 @@
 
 namespace HotelSmartManagement.HotelOverview.MVVM.ViewModels
 {
-    public class HotelManagementDashboardViewModel : ViewModelBase
+    public class HotelManagementDashboardViewModel : ViewModelWithMessenging
     {
         public HotelManagementDashboardViewModel(Globals globals) : base(globals)
         {
         }
+
+        public override string Name => nameof(HotelManagementDashboardViewModel);
     }
 }
