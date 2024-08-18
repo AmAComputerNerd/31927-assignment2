7573b3593f39f58c4ffd432ea827f2a8c7de70fd
Lachlan Giang <92299122+LachlanGiang@users.noreply.github.com>
2024-08-19 00:38:13 +1000

    LGN/binding (#25)
    Co-authored-by: Lachlan Giang <lachlan.giang@wisetechglobal.com>

--- HotelSmartManagement/HotelOverview/MVVM/ViewModels/AddEventViewModel.cs
+++ HotelSmartManagement/HotelOverview/MVVM/ViewModels/AddEventViewModel.cs
@@ -1,12 +1,15 @@
 ﻿using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
 using HotelSmartManagement.Common.Database.Services;
+using HotelSmartManagement.Common.Events;
 using HotelSmartManagement.Common.MVVM.Models;
+using HotelSmartManagement.Common.MVVM.ViewModels;
 using HotelSmartManagement.HotelOverview.MVVM.Models;
 using System.Collections.ObjectModel;
 
 namespace HotelSmartManagement.HotelOverview.MVVM.ViewModels
 {
-    public class AddEventViewModel : ViewModelWithMessenging
+    public class AddEventViewModel : AddEntityViewModel
     {
         public override string Name => nameof(AddEventViewModel);
 
@@ -24,23 +27,25 @@ namespace HotelSmartManagement.HotelOverview.MVVM.ViewModels
         public AsyncRelayCommand OnCancel_Clicked { get; }
 
 #pragma warning disable CS8618 // Reason: private fields are set through public properties.
-        public AddEventViewModel(Globals globals) : base(globals)
+        public AddEventViewModel(Globals globals, HotelOverviewService hotelOverviewService) : base(globals)
 #pragma warning restore CS8618 // Reason: private fields are set through public properties.
         {
-            //ImageUri = Globals.GetProfilePictureUri();
+            _hotelOverviewService = hotelOverviewService;
+
+            _areaTypes = [.. Enum.GetNames(typeof(Area))];
+            ErrorMessage = string.Empty;
+            OnSaveClose_Clicked = new AsyncRelayCommand(async () => await Task.Run(() => OnSave()));
+            OnCancel_Clicked = new AsyncRelayCommand(async () => await Task.Run(() => Messenger.Send(new ChangeViewEvent(typeof(HotelOverviewDashboardViewModel)), nameof(MainViewModel))));
         }
 
-        public void OnCancel()
+        async public override void OnSave()
         {
-            throw new NotImplementedException();
-        }
-        public void OnSave()
-        {
-            throw new NotImplementedException();
-        }
-        public void ValidateFields()
-        {
-            throw new NotImplementedException();
+            if (ValidateFields())
+            {
+                _hotelOverviewService.NewEvent(Title, Details, AreaAffected);
+                await Task.Run(() => Messenger.Send(new ChangeViewEvent(typeof(HotelOverviewDashboardViewModel)), nameof(MainViewModel)));
+            }
+
         }
     }
 }
--- HotelSmartManagement/HotelOverview/MVVM/ViewModels/ManageInventoryViewModel.cs
+++ HotelSmartManagement/HotelOverview/MVVM/ViewModels/ManageInventoryViewModel.cs
@@ -1,30 +1,61 @@
-﻿using HotelSmartManagement.Common.MVVM.Models;
+﻿using CommunityToolkit.Mvvm.Input;
+using HotelSmartManagement.Common.Database.Services;
+using HotelSmartManagement.Common.Events;
+using HotelSmartManagement.Common.MVVM.Models;
+using HotelSmartManagement.Common.MVVM.ViewModels;
+using HotelSmartManagement.HotelOverview.MVVM.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace HotelSmartManagement.HotelOverview.MVVM.ViewModels
 {
-    public class ManageInventoryViewModel : ViewModelBase
+    public class ManageInventoryViewModel : ViewModelWithMessenging
     {
-        private Uri _imageUri;
-        public Uri ImageUri { get => _imageUri; set => SetProperty(ref _imageUri, value); }
+        public override string Name => nameof(ManageInventoryViewModel);
+
+        private HotelOverviewService _hotelOverviewService;
+        private ObservableCollection<InventoryItem> inventoryItems;
+        private double _userTimeRecording;
+
+        // Public properties.
+        public ObservableCollection<InventoryItem> InventoryItems {  get; set; }
+        // Commands
+        public ICommand IncreaseQuantityCommand { get; }
+        public ICommand DecreaseQuantityCommand { get; }
+        public AsyncRelayCommand OnClose_Clicked { get; }
 
 #pragma warning disable CS8618 // Reason: private fields are set through public properties.
-        public ManageInventoryViewModel(Globals globals) : base(globals)
+        public ManageInventoryViewModel(Globals globals, HotelOverviewService hotelOverviewService) : base(globals)
 #pragma warning restore CS8618 // Reason: private fields are set through public properties.
         {
-            //ImageUri = Globals.GetProfilePictureUri();
+            _hotelOverviewService = hotelOverviewService;
+            InventoryItems = new ObservableCollection<InventoryItem>(_hotelOverviewService.GetAllInventory() ?? Array.Empty<InventoryItem>());
+
+            IncreaseQuantityCommand = new RelayCommand<InventoryItem>(IncreaseQuantity);
+            DecreaseQuantityCommand = new RelayCommand<InventoryItem>(DecreaseQuantity);
+
+            OnClose_Clicked = new AsyncRelayCommand(async () => await Task.Run(() => OnClose()));
+        }
+        async public void OnClose()
+        {
+            await Task.Run(() => Messenger.Send(new ChangeViewEvent(typeof(HotelOverviewDashboardViewModel)), nameof(MainViewModel)));
+        }
+        private void IncreaseQuantity(InventoryItem item)
+        {
+            if (item != null)
+            {
+                item.Quantity++;
+                _hotelOverviewService.UpdateInventoryItem(item);
+            }
         }
 
-        public void OnCancel()
+        private void DecreaseQuantity(InventoryItem item)
         {
-            throw new NotImplementedException();
-        }
-        public void OnSave()
-        {
-            throw new NotImplementedException();
-        }
-        public void ValidateFields()
-        {
-            throw new NotImplementedException();
+            if (item != null && item.Quantity > 0)
+            {
+                item.Quantity--;
+                _hotelOverviewService.UpdateInventoryItem(item);
+            }
         }
     }
 }
